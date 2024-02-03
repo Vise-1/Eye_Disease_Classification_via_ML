95d7baedfc79a536937457f9a424e7b1763adbf2
Vise1 <sparsh1711.alive@gmail.com>
2024-02-03 20:42:11 +0530

    Create functions.py
    Essential Functions for easier to understand and cleaner code overall.

---
+++ functions.py
@@ -0,0 +1,4 @@
+def walk_through_dir(dir_path):
+  """Walks through dir_path returning its contents."""
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
